1471709cf8fbabb11c21afa50ebc7fee941ccd7d
Hemant Baviskar <hemantyb22@gmail.com>
2026-04-04 23:09:44 +0530

    configured keymap for neovim

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -1,2 +1,11 @@
+local fzf = require("fzf-lua")
+
 vim.keymap.set("n", "<Leader>e", ":NvimTreeFocus<CR>")
+vim.keymap.set("n", "<Leader>x", ":BufferClose<CR>")
 vim.keymap.set("n", "<C-n>", ":NvimTreeClose<CR>")
+
+vim.keymap.set("n", "<leader>ff", fzf.files, { desc = "Fzf Files" })
+vim.keymap.set("n", "<leader>fg", fzf.live_grep, { desc = "Fzf Live Grep" })
+vim.keymap.set("n", "<leader>fb", fzf.buffers, { desc = "Fzf Buffers" })
+
+vim.keymap.set("n", "<leader>gs", fzf.git_status, { desc = "Git Status" })
