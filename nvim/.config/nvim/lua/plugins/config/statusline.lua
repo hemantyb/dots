8fdef58672dde4f4d72941f0f3dbb378d10023cb
Hemant Baviskar <hemantyb22@gmail.com>
2026-04-05 10:35:28 +0530

    change: to rainbow style for slimline

--- nvim/.config/nvim/lua/plugins/config/statusline.lua
+++ nvim/.config/nvim/lua/plugins/config/statusline.lua
@@ -1,1 +1,19 @@
-require("slimline").setup({})
+require("slimline").setup({
+	configs = {
+		path = {
+			hl = {
+				primary = "Define",
+			},
+		},
+		git = {
+			hl = {
+				primary = "Function",
+			},
+		},
+		filetype_lsp = {
+			hl = {
+				primary = "String",
+			},
+		},
+	},
+})
